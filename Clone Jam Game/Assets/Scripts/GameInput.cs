8da8bfe9023ac2a3a47b99ce44b0a66afa95899d
CilekMafya <derinfaydali@gmail.com>
2026-02-07 19:12:35 +0300

    main menu

--- Clone Jam Game/Assets/Scripts/GameInput.cs
+++ Clone Jam Game/Assets/Scripts/GameInput.cs
@@ -1,31 +1,62 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
-    public DialogueBox dialogueBox;
+    // --- EVENTLER ---
+    // Karýþýklýðý önlemek için ismini deðiþtirdim:
     public event EventHandler OnInteractAction;
+    public event EventHandler OnPauseToggle;
 
+    // --- DEÐÝÞKENLER ---
+    [SerializeField] private DialogueBox dialogueBox; // Inspector'dan atamayý unutma
     private PlayerInputActions playerInputActions;
+
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
-        playerInputActions.Player.Interact.performed += Interact_performed;
+        // Etkileþim Tuþu Baðlantýsý
+        playerInputActions.Player.Interact.performed += Interact_Performed;
+
+        playerInputActions.Player.Pause.performed += Pause_Performed;
     }
 
-    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void Pause_Performed(InputAction.CallbackContext obj)
     {
-        if(dialogueBox == null || !dialogueBox.gameObject.activeSelf) OnInteractAction?.Invoke(this, EventArgs.Empty);
+        // Event'i tetikle (PauseToggle ismini kullanýyoruz)
+        OnPauseToggle?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Interact_Performed(InputAction.CallbackContext obj)
+    {
+        // Diyalog kutusu yoksa veya kapalýysa etkileþime izin ver
+        if (dialogueBox == null || !dialogueBox.gameObject.activeSelf)
+        {
+            OnInteractAction?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public Vector2 GetmovementVector()
     {
-        if(dialogueBox != null && dialogueBox.gameObject.activeSelf) return Vector2.zero;
+        // Diyalog açýksa hareket etme
+        if (dialogueBox != null && dialogueBox.gameObject.activeSelf)
+        {
+            return Vector2.zero;
+        }
+
+        // Hareket verisini oku
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector;
+    }
+
+    // Hafýza temizliði (Memory Leak önlemek için þart)
+    private void OnDestroy()
+    {
+        playerInputActions.Player.Interact.performed -= Interact_Performed;
+        playerInputActions.Player.Pause.performed -= Pause_Performed;
+        playerInputActions.Dispose();
     }
 }
