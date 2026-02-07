0583840b7c6b0829b313859c82179a51c462b924
Ahmet Emir <ahmetem8rulu1991@gmail.com>
2026-02-07 06:17:20 +0300

    NPC3 Dialogue Integration

--- Clone Jam Game/Assets/Room3/Scripts/Room3NPCController.cs
+++ Clone Jam Game/Assets/Room3/Scripts/Room3NPCController.cs
@@ -2,31 +2,26 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEditor.Rendering.MaterialUpgrader;
 
-public class Room3NPSController : InteractableObject
+public class Room3NPCController : InteractableObject
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public int upper_y;
     public int lower_y;
     public float speed;
-    public string NpcName;
-    public string dialogueText;
-
-    public bool speaking;
-
-
+    public DialogueBox dialogueBox;
+    public Dialogue dialogue;
     private int direction;
 
     void Start()
     {
         direction = -1;
-        speaking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!speaking) {
+        if (!dialogueBox.gameObject.activeSelf) {
             transform.position = transform.position + (new Vector3(0, 1, 0)) * direction * speed * Time.deltaTime;
             if (transform.position.y >= upper_y)
             {
@@ -42,19 +37,7 @@ public class Room3NPSController : InteractableObject
 
     public override void Interact()
     {
-        Debug.Log($"Talking to {NpcName}: {dialogueText}");
         //TODO
-        StartSpeak();
-    }
-
-    // CALL THESE FUNCTIONS FROM PLAYER
-    public void StartSpeak() {
-        speaking = true;
-        //TODO
-    }
-
-    public void StopSpeaking() {
-        speaking = false;
-        //TODO
+        dialogueBox.StartDialogue(dialogue);
     }
 }
